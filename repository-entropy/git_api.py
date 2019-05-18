c9762b447a7babf76f9269403f3f52658ee2d3ee
Lars Egholm Pedersen <larsegholm@gmail.com>
2019-05-18 14:01:15 +0200

    Added diff class / hide members / methods in git_api

--- repository-entropy/git_api.py
+++ repository-entropy/git_api.py
@@ -3,32 +3,33 @@ import requests as req
 
 class GitApi:
 
-    git_api_version = "v3"
-    git_url = ""
-    owner = ""
-    repo = ""
+    # Need to create some integration tests for this
+    __git_api_version = "v3"
+    __git_url = ""
+    __owner = ""
+    __repo = ""
 
     def __init__(self, git_url, owner, repo):
-        self.git_url = git_url
-        self.owner = owner
-        self.repo = repo
+        self.__git_url = git_url
+        self.__owner = owner
+        self.__repo = repo
 
     def __str__(self):
-        return "Entry-point : " + self.form_request([])
+        return "Entry-point : " + self.__form_request([])
 
-    def form_request(self, api_paths):
-        return '/'.join([self.git_url]+["repos"]+[self.owner]+[self.repo]+api_paths)
+    def __form_request(self, api_paths):
+        return '/'.join([self.__git_url] + ["repos"] + [self.__owner] + [self.__repo] + api_paths)
 
     def get_commit_shas(self):
-        request_url = self.form_request(["commits"])
-        accept_header = "application/vnd.github." + self.git_api_version + ".json"
+        request_url = self.__form_request(["commits"])
+        accept_header = "application/vnd.github." + self.__git_api_version + ".json"
         package = req.get(request_url, accept_header)
         assert(package.status_code == 200)
         return [commit["sha"] for commit in package.json()] # Hmmm, refine this when doing analysis
 
     def get_diff(self, commit_sha):
-        request_url = self.form_request(["commits", commit_sha])
-        accept_header = "application/vnd.github."+self.git_api_version+".diff"
+        request_url = self.__form_request(["commits", commit_sha])
+        accept_header = "application/vnd.github." + self.__git_api_version + ".diff"
         package = req.get(request_url, headers={"Accept":accept_header})
         assert (package.status_code == 200)
         return package.text
